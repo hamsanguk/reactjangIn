4b01ab84874c0aafd40b01c919d972b464ee6a1e
hamsanguk <gtd0906@gmail.com>
2023-01-19 00:29:37 +0900

    개별따봉

--- src/App.js
+++ src/App.js
@@ -2,103 +2,48 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  let [글제목,글제목변경]=useState(['강남우동맛집','남자 코트 세일','여자 코트 세일','작성해본 네번째']);
+  let [따봉,따봉변경]=useState([0,0,0,0]);
+  let [modal,setModal]=useState(true);
 
-  let post = 'hosting service'
-  let [a,aTitle] = useState(['useState 사용','두번째사용','세번째 사용'])
-  let [like, likeplus ]=useState(0)
-  let [modal,setModal]=useState(false)
 
-  like.map(
-    function(a,i){
-      let copyLike = [...like]
-      
-    }
-  )
-  
-
-  return (
-    <div className="App">
+  return(
+    <div className='App'>
       <div className='black-nav'>
         <h4>blog</h4>
       </div>
-      
-      <button onClick={()=>{
-       let aCopy = [...a];
-       aCopy[0]='stateFunction사용'
-       aTitle(aCopy)
-      }}>변경</button>
-
-      <button onClick={()=>{
-        let alignACoppy=[...a];
-        alignACoppy.sort()
-        aTitle(alignACoppy)
-      }}>글자순정렬</button>
-
-      {/* <div className='list'>
-        <h4>{a[0]} <span onClick={()=>{likeplus(like+1)}}>👍</span>{like}</h4>
-        <p>
-          Dec.24 2021
-        </p>
-      </div>
-      <div className='list'>
-        <h4>{a[1]}</h4>
-        <p>
-          Dec.24 2021
-        </p>
-      </div>
-      <div className='list'>
-        <h4 onClick={()=>{
-          setModal(
-            modal==true?modal=false:modal=true
-            //!modal
-          )
-        }}>{a[2]}</h4>
-        <p>
-          Dec.24 2021
-        </p>
-      </div>
- */}
-
       {
-        a.map(function(ad,anum){
+        글제목.map((i,n)=>{
           return(
-          <div className='list'>
-        <h4 onClick={()=>{
-          setModal(
-            // !modal
-            modal==true?modal=false:true
-          )
-        }}>
-          {a[anum]}
-          <span onClick={()=>{likeplus(like+1)}}>
-            👍
-          </span>{like}</h4>
-        
-        <p>
-          Dec.24 2021
-        </p>
-      </div>
-          )
+        <div className='list' key={n}>
+          <h5 onClick={()=>{
+            setModal(!modal)
+          }}>{i}</h5>
+
+          <span onClick={()=>{
+            let copy=[...따봉];
+            copy[n]=copy[n]+1
+            따봉변경(copy)        
+          }}>👍{따봉[n]}</span>
+
+          <p>대충 날짜들 적음</p>
+        </div>
+          ) 
         })
       }
-
-      {
-        modal==true?<Modal/>:null
-      }
-
+      {modal==true?<Modal/>:null}
     </div>
-    //scss confirm
+    
   );
 }
 
 function Modal(){
   return(
     <div className='modal'>
-        <h4 >Title</h4>
-        <p><i>date</i></p>
-        <p>상세내용</p>
-      </div>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
-
 export default App;
